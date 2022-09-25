1d363ae5d1b18e919961fa2583c1ae6dbcff3ff1
Gongsta <gong.steven@hotmail.com>
2022-09-24 21:12:03 -0400

    add Meta Hacker Cup

---
+++ Facebook/2022/round2/B2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int tt;
+    cin >> tt;
+    for (int t=1;t<=tt;t++) {
+        int n;
+        cin >> n;
+        int ans = 0;
+        cout << "Case #" << t << ": " << ans << endl;
+    }
+
+    return 0;
+}
